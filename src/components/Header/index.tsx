19b476978d5e9bc229024214be1e64f2caf3afe8
21ciprian <2021ciprianoffice@gmail.com>
2022-03-20 15:25:32 +0000

    Styled app

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './Header.module.css'
 
 type HeaderProps = {
 	title: string
 }
 
 function Header({title}: HeaderProps) {
-	return <header>{title}</header>
+	return <header className={styles.header}>{title}</header>
 }
 
 export default Header
