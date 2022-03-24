3701624eadb423eb526c43db2b7299ba9a0fff85
21ciprian <2021ciprianoffice@gmail.com>
2022-03-24 23:49:40 +0000

    Imported Logout in App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,6 +4,7 @@ import Balance from '../Balance'
 import Header from '../Header'
 import IncomeExpenses from '../IncomeExpenses'
 import LoginButton from '../Login'
+import LogoutButton from '../Logout'
 import TransactionList from '../TransactionList'
 import './App.css'
 function App(): JSX.Element {
@@ -11,6 +12,7 @@ function App(): JSX.Element {
 		<GlobalProvider>
 			<article className='app'>
 				<LoginButton />
+				<LogoutButton />
 				<Header title='Expense Tracker' />
 				<Balance />
 				<IncomeExpenses />
