17919ed6dc9d0038f7349f2d7ba99e4db10e4f73
21ciprian <2021ciprianoffice@gmail.com>
2022-03-24 23:36:06 +0000

    Imported login Button to App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -3,12 +3,14 @@ import AddTransaction from '../AddTransaction'
 import Balance from '../Balance'
 import Header from '../Header'
 import IncomeExpenses from '../IncomeExpenses'
+import LoginButton from '../Login'
 import TransactionList from '../TransactionList'
 import './App.css'
 function App(): JSX.Element {
 	return (
 		<GlobalProvider>
 			<article className='app'>
+				<LoginButton />
 				<Header title='Expense Tracker' />
 				<Balance />
 				<IncomeExpenses />
