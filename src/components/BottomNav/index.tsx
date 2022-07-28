8057d5bd62e73e4ee98db2260976e8b1527d3200
21ciprian <2021ciprianoffice@gmail.com>
2022-07-28 23:15:09 +0100

    Added icons to  bottom nav

--- src/components/BottomNav/index.tsx
+++ src/components/BottomNav/index.tsx
@@ -1,8 +1,22 @@
+import {
+	AiOutlineHistory,
+	AiOutlineHome,
+	AiOutlinePlusCircle
+} from 'react-icons/ai'
+import {BsGraphDown, BsGraphUp} from 'react-icons/bs'
 import styles from './BottomNav.module.css'
 type Props = {}
 
 const index = (props: Props) => {
-	return <div className={styles.bottomNav}>index</div>
+	return (
+		<div className={styles.bottomNav}>
+			<AiOutlineHome />
+			<AiOutlineHistory />
+			<AiOutlinePlusCircle />
+			<BsGraphDown />
+			<BsGraphUp />
+		</div>
+	)
 }
 
 export default index
